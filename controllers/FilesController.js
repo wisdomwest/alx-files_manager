75e79db249c64a99f441374a657b5fc705af6138
wisdomwest <harmonykoome@gmail.com>
2024-05-12 01:47:20 +0300

    task 6 lets see

--- controllers/FilesController.js
+++ controllers/FilesController.js
@@ -86,6 +86,58 @@ class FilesController {
       parentId,
     });
   }
+
+  static async getShow(req, res) {
+    const token = req.header('X-Token');
+    if (!token) return res.status(401).send({ error: 'Unauthorized' });
+
+    const userId = await redisClient.get(`auth_${token}`);
+    const user = await dbClient.users.findOne({ _id: ObjectId(userId) });
+    if (!user) return res.status(401).send({ error: 'Unauthorized' });
+
+    const fileId = req.params.id;
+    if (!fileId) return res.status(404).send({ error: 'Not found' });
+
+    const file = await dbClient.files.findOne({ _id: ObjectId(fileId) });
+    if (!file) return res.status(404).send({ error: 'Not found' });
+
+    if (file.userId.toString() !== userId) return res.status(404).send({ error: 'Not found' });
+
+    return res.status(200).json({
+      id: file._id,
+      userId: file.userId,
+      name: file.name,
+      type: file.type,
+      isPublic: file.isPublic,
+      parentId: file.parentId,
+    });
+  }
+
+  static async getIndex(req, res) {
+    const token = req.header('X-Token');
+    if (!token) return res.status(401).send({ error: 'Unauthorized' });
+
+    const page = req.query.page ? parseInt(req.query.page, 10) : 0;
+
+    const userId = await redisClient.get(`auth_${token}`);
+    const user = await dbClient.users.findOne({ _id: ObjectId(userId) });
+    if (!user) return res.status(401).send({ error: 'Unauthorized' });
+
+    const parentId = req.query.parentId ? req.query.parentId : 0;
+    if (parentId !== 0) {
+      const parent = await dbClient.files.findOne({ _id: ObjectId(parentId) });
+      if (!parent) return res.status(400).send({ error: 'Parent not found' });
+      if (parent.type !== 'folder') return res.status(400).send({ error: 'Parent is not a folder' });
+    }
+
+    const files = await dbClient.files
+      .find({ userId, parentId })
+      .skip(page * 20)
+      .limit(20)
+      .toArray();
+
+    return res.status(200).json(files);
+  }
 }
 
 export default FilesController;
